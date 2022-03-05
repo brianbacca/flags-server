0f3955bfd9a3c05232e5f82aa09d925dbc483a11
brianbacca <brianbn05@gmail.com>
2022-03-05 15:28:56 -0300

    config and dependency graphql

---
+++ src/core/graphql/graphql.module.ts
@@ -0,0 +1,15 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      introspection: true,
+      autoSchemaFile: join(process.cwd(), 'src/core/graphql/api-schema.gql'),
+    }),
+  ],
+})
+export class GraphqlModule {}
